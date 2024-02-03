30b1a56c69ada48caa8164f7905245eb25f55d60
iarley <7iarley7@gmail.com>
2024-02-03 15:05:21 -0300

    Feat: Define parametro para rotas

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -6,8 +6,22 @@ class Core
 {
     public static function dispath(array $routes)
     {
-        echo "<pre>";
-        print_r($routes);
-        echo "</pre>";
+        // base url
+        $url = '/';
+
+        // se url 'setado' e concatena a url
+        isset($_GET['url']) && $url .= $_GET['url'];
+
+        foreach ($routes as $route) {
+            // pasando parametros, preg_replace vai troacr o {id} para o id passado na rota
+            $pattern = '#^' . preg_replace('/{id}/', '(\w+)', $route['path']) . '$#';
+
+            // 'match' na rota encontrada, com base no parametro
+            if (preg_match($pattern, $url, $params)) {
+                // esse params retorna um array, sendo o primeiro valor a rota, segundo valor o id passado no parametro sendo assim para pegar apenas o valor de parametro, eu 'corto' o primeiro valor com: 
+                array_shift($params);
+                var_dump($params);
+            }
+        }
     }
 }
