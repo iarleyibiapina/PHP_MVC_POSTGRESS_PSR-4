9faec6be4d685411cbbcbbd959795e72d73d1b30
iarley <7iarley7@gmail.com>
2024-02-03 15:54:13 -0300

    Feat: Cria classe response

---
+++ src/Http/Response.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http;
+
+class Response
+{
+    // response em json
+    public static function json(array $data = [], int $statusCode = 200, array $headers = ['Content-Type' => 'application/json'])
+    {
+        http_response_code(200);
+
+        foreach ($data as $key => $value) {
+            header($key . ":" . $value);
+        }
+
+        echo json_encode($data);
+    }
+    public static function setHeaders(array $headers = ['Content-Type' => 'application/json'])
+    {
+        foreach ($headers as $key => $value) {
+            header($key . ":" . $value);
+        }
+    }
+    public static function setStatusCode(int $statusCode = 200)
+    {
+        http_response_code($statusCode);
+    }
+    public static function redirect(string $url)
+    {
+        header("Location: " . $url);
+    }
+}
